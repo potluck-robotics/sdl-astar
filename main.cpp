88445ead07e90cf4c9d104b1d9c6a3deb5e16344
Seongyoon Cho <seongyoon.cho@ixdl.kr>
2023-10-14 16:14:15 +0900

    Draw grid by fill rectangle

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <SDL.h>
 
-int grid[10][10];
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+#define GRID_WIDTH 12 
+#define GRID_HEIGHT 9 
+
+int grid[GRID_WIDTH][GRID_HEIGHT];
 void init_grid();
-void draw_grid(SDL_Renderer* renderer);
+void draw_grid(SDL_Renderer* renderer, const int cell_size);
 
 int main(int argc, char* argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* window = SDL_CreateWindow("SDL2 Window",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        680, 480, 0);
+        WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
@@ -23,7 +29,8 @@ int main(int argc, char* argv[])
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        draw_grid(renderer);
+        unsigned int cell_size = 30;
+        draw_grid(renderer, cell_size);
         while (SDL_PollEvent(&e)){
             if (e.type == SDL_QUIT){
                 quit = true;
@@ -46,20 +53,25 @@ int main(int argc, char* argv[])
 
 void init_grid()
 {
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < GRID_WIDTH; i++){
+        for (int j = 0; j < GRID_HEIGHT; j++) {
             grid[i][j] = 0;
         }
     }
 }
 
-void draw_grid(SDL_Renderer* renderer)
+void draw_grid(SDL_Renderer* renderer, const int cell_size)
 {
     SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-    SDL_RenderDrawLine(renderer, 100, 100, 200, 200); 
 
-//    for(int i = 0; i < 10 ; i++) {
-//        SDL_RenderDrawLine(renderer, i*48, 0, i*48, 680); 
-//        SDL_RenderDrawLine(renderer, 0, i*68, 480, i*68); 
-//    }
+    for(int i = 0; i < GRID_WIDTH ; i++) 
+    {
+        for(int j = 0; j < GRID_HEIGHT ; j++) 
+        {
+            int x = (WINDOW_WIDTH - cell_size * GRID_WIDTH) / 2 + i * cell_size + 1;
+            int y = (WINDOW_HEIGHT - cell_size * GRID_HEIGHT) / 2 + j * cell_size + 1;
+            SDL_Rect rect = {x, y, cell_size - 2, cell_size - 2};
+            SDL_RenderFillRect(renderer, &rect);
+        }
+    }
 }
