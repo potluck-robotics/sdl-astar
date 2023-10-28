3170c87651cb352157f84ee55dc86b19809c2658
Donghee Park <dongheepark@gmail.com>
2023-10-28 10:20:43 +0900

    Add Doxygen comments

--- robot.h
+++ robot.h
@@ -1,11 +1,40 @@
 #pragma once
 #include "world.h"
 
+/*! @brief The Robot class
+ *
+ *  This class represents the robot in the game.
+ *  It is a simple object that can move around the world.
+ */
 class Robot {
     public:
+
+        /*! @brief Constructor for the Robot class
+         *
+         *  @param world The world in which the robot exists
+         */ 
         Robot(World* world);
+
+        /*! @brief Move the robot
+         *
+         *  This function moves the robot by the given amount.
+         *
+         *  @param dx The amount to move in the x direction
+         *  @param dy The amount to move in the y direction
+         */
         void move(const int dx, const int dy);
+
+        /*! @brief Draw the robot
+         *
+         *  This function draws the robot to the screen.
+         *
+         *  @param renderer The SDL_Renderer to draw to
+         *  @param window_w The width of the window
+         *  @param window_h The height of the window
+         *  @param cell_size The size of each cell in the grid
+         */
         void draw(SDL_Renderer* renderer, const int window_w, const int window_h, const int cell_size);
+
     private:
         World* world_;
         int x_;
